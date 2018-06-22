66d14d76b52446f19356de290a12fa4d2076dab2
creativityjohn <john.tsutsui@creativityincorporated.com>
2018-06-22 00:52:43 -0400

    add comments to chapter 4, exercise 14

--- chapter4/exercise04_14.c
+++ chapter4/exercise04_14.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-#define swap(type, x, y) { type z; z = x; x = y; y = z; } 
+// don't need pointers because this is not a function and is inserted "inline"
+#define swap(t, x, y) { t z; z = x; x = y; y = z; } 
 
 void main()
 {
@@ -9,6 +10,8 @@ void main()
     x = 1;
     y = 2;
     printf("x = %d, y = %d\n", x, y);
+    // don't need pointers because this is not a function and is inserted "inline"
+    // note that I don't think our cc65 compiler allows declarations that are not at the top of a function
     swap(int, x, y);
     printf("x = %d, y = %d\n", x, y);
 }
