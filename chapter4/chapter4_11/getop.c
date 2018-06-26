addaa259ecb9b94633ec98d307c4a10e6be9d4cd
creativityjohn <john.tsutsui@creativityincorporated.com>
2018-06-26 02:22:42 -0400

    chapter4, exercise 11

--- chapter4/chapter4_11/getop.c
+++ chapter4/chapter4_11/getop.c
@@ -1,18 +1,38 @@
+// Instead of using ungetch(), use an internal static variable.
+// ungetch() reverts the position of the last get so we can get it again the next go-round.
+// For example, 5 2+ will unget the + so we can get the + the next time.
+// Therefore, to replace ungetch(), we need to save the currentChar in a static variable.
+// Whenever we normally call getch(), we need to use the value in currentChar instead and
+// then check the value in getch() to initiate the next go-round.
+
 #include <ctype.h>
 #include <stdio.h>
 #include "calc.h"
 
 int getch(void);
-void ungetch(int);
+// void ungetch(int);
 
 // getop: get next operator or numerand
 int getop(char s[])
 {
     int i, c;
-    
-    while ((s[0] = c = getch()) == ' ' || c == '\t')
+    static int currChar = -1;       // -1 is unitialized
+
+    // Whenever we normally call getch(), we need to use the value in currentChar instead and
+    // then check the value in getch() to initiate the next go-round.
+    if (currChar == -1)
     {
-        // do nothing
+        c = getch();
+    }
+    else
+    {
+        c = currChar;
+        currChar = -1;
+    }
+    
+    while ((s[0] = c) == ' ' || c == '\t')
+    {
+        c = getch();
     }
     s[1] = '\0';
     if (!isdigit(c) && c != '.')
@@ -37,7 +57,8 @@ int getop(char s[])
     s[i] = '\0';
     if (c != EOF)
     {
-        ungetch(c);
+        //ungetch(c);
+        currChar = c;
     }
     return NUMBER;
 }
